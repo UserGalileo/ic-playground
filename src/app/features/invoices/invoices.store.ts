2cdfa89d9bbafeb6729772b0dd1eeac5bc62b3fd
Michele Stieven <usergalileo@gmail.com>
2023-07-11 17:28:59 +0200

    end

--- src/app/features/invoices/invoices.store.ts
+++ src/app/features/invoices/invoices.store.ts
@@ -1,53 +1,76 @@
 import { inject, Injectable } from '@angular/core';
 import { InvoicesService } from '../../api/services/invoices.service';
 import { ClientsService } from '../../api/services/clients.service';
-import { BehaviorSubject, forkJoin } from 'rxjs';
+import { concatMap, exhaustMap, forkJoin, map, switchMap, tap } from 'rxjs';
 import { Client, Invoice } from '../../api/models';
+import { ComponentStore } from '@ngrx/component-store';
+
+interface InvoicesState {
+  invoices: Invoice[];
+  clients: Client[];
+}
 
 @Injectable()
-export class InvoicesStore {
+export class InvoicesStore extends ComponentStore<InvoicesState>{
 
   // Deps
   private invoicesService = inject(InvoicesService);
   private clientsService = inject(ClientsService);
 
   // States
-  invoices$ = new BehaviorSubject<Invoice[]>([]);
-  clients$ = new BehaviorSubject<Client[]>([]);
+  invoices$ = this.select(state => state.invoices);
+  clients$ = this.select(state => state.clients);
 
-  loadInvoices() {
-    forkJoin([
-      this.invoicesService.loadInvoices(),
-      this.clientsService.loadClients()
-    ]).subscribe(([invoices, clients]) => {
-      this.invoices$.next(invoices);
-      this.clients$.next(clients);
-    })
+  constructor() {
+    super({
+      invoices: [],
+      clients: []
+    });
   }
 
-  createInvoice() {
-    this.invoicesService.addInvoice({
+  loadInvoices = this.effect($ => $.pipe(
+    exhaustMap(() => forkJoin([
+      this.invoicesService.loadInvoices(),
+      this.clientsService.loadClients()
+    ]).pipe(
+      tap(([invoices, clients]) => {
+        this.setState({ invoices, clients });
+      })
+    )),
+  ));
+
+  createInvoice = this.effect($ => $.pipe(
+    concatMap(() => this.invoicesService.addInvoice({
       subject: '',
       total: 0,
       items: []
-    }).subscribe(invoice => {
-      this.invoices$.next([ ...this.invoices$.getValue(), invoice ]);
-    })
-  }
+    }).pipe(
+      tap(invoice => {
+        this.setState(state => ({ ...state, invoices: state.invoices.concat(invoice) }))
+      })
+    )),
+  ));
 
-  saveInvoice(invoice: Invoice) {
-    this.invoicesService.editInvoice(invoice).subscribe(newInvoice => {
-      this.invoices$.next(
-        this.invoices$.getValue().map(i => i.id === invoice.id ? newInvoice : i)
-      )
-    })
-  }
+  saveInvoice = this.effect<Invoice>(invoice$ => invoice$.pipe(
+    switchMap(invoice => this.invoicesService.editInvoice(invoice).pipe(
+      tap(newInvoice => {
+        this.setState(state => ({
+          ...state,
+          invoices: state.invoices.map(i => i.id === newInvoice.id ? newInvoice : i)
+        }));
+      })
+    )),
+  ));
 
-  deleteInvoice(id: string) {
-    this.invoicesService.deleteInvoice(id).subscribe(() => {
-      this.invoices$.next(
-        this.invoices$.getValue().filter(i => i.id !== id)
-      )
-    })
-  }
+  deleteInvoice = this.effect<string>(invoiceId$ => invoiceId$.pipe(
+    concatMap(id => this.invoicesService.deleteInvoice(id).pipe(
+      map(() => id),
+      tap(id => {
+        this.setState(state => ({
+          ...state,
+          invoices: state.invoices.filter(i => i.id !== id)
+        }));
+      }),
+    )),
+  ))
 }
